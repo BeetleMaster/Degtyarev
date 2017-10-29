e67f46f18565d1befe83450525bc115e997a25bf
BeetleMaster <valapavac@mail.ru>
2017-10-29 16:30:39 +1000

    Add files via upload

---
+++ Prac3/Practic3.py
@@ -0,0 +1,34 @@
+'''Дан файл с расписанием занятий на неделю.
+Помимо названия предмета в нем также указано лекция это, или практическое занятие, или лабораторная работа.
+ В одной строке может быть указаны только один предмет с информацией о нем.
+ Посчитать, сколько за неделю проходит практических занятий, лекций и лабораторных работ.'''
+
+
+class EmptyFileException(Exception):
+    pass
+
+
+with open("week.txt", "r") as f:
+    data = f.read().split()
+
+lecture = 0
+lab = 0
+practice = 0
+try:
+    if not data:
+        raise EmptyFileException("Файл пуст")
+except EmptyFileException as empty:
+    print(empty)
+else:
+    for i in data:
+        if i == '(лекц.)':
+            lecture += 1
+        if i == '(практ.)':
+            practice += 1
+        if i == '(лаб.)':
+            lab += 1
+
+with open("output.txt", "w") as output:
+    print("Количество лекций: {}".format(lecture), file = output)
+    print("Количество практик: {}".format(practice), file = output)
+    print("Количество лабораторных работ: {}".format(lab), file = output)
